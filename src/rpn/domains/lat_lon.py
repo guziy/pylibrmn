fd06b81c2bebf6b47673c52b443baa80cea7dec8
Huziy Oleksandr <guziy.sasha@gmail.com>
2014-06-09 15:42:35 -0400

    add rotated latlon integration

---
+++ src/rpn/domains/lat_lon.py
@@ -0,0 +1,76 @@
+import math
+
+__author__ = "huziy"
+__date__ = "$13 juil. 2010 13:34:52$"
+
+from math import atan2
+import numpy as np
+
+EARTH_RADIUS_METERS = 0.637122e7  # mean earth radius used in the CRCM5 model for area calculation
+
+#longitude and latitude are in radians
+def get_nvector(rad_lon, rad_lat):
+    return [np.cos(rad_lat) * np.cos(rad_lon), np.cos(rad_lat) * np.sin(rad_lon), np.sin(rad_lat)]
+
+
+#p1 and p2 are geopoint objects
+def get_distance_in_meters(*arg):
+    """
+    arg = point1, point2
+    arg = lon1, lat1, lon2, lat2
+    """
+    if len(arg) == 2:  # if we have 2 geopoints as an argument
+        [p1, p2] = arg
+        n1 = p1.get_nvector()
+        n2 = p2.get_nvector()
+    elif len(arg) == 4:  # if we have the coordinates of two points in degrees
+        x = np.radians(arg)
+        n1 = get_nvector(x[0], x[1])
+        n2 = get_nvector(x[2], x[3])
+    else:
+        raise Exception("get_distance_in_meters should be 2 or 4 parameters.")
+    return EARTH_RADIUS_METERS * get_angle_between_vectors(n1, n2)
+
+
+def get_angle_between_vectors(n1, n2):
+    dy = np.cross(n1, n2)
+    dy = np.dot(dy, dy) ** 0.5
+    dx = np.dot(n1, n2)
+    return atan2(dy, dx)
+
+
+def lon_lat_to_cartesian(lon, lat, R=EARTH_RADIUS_METERS):
+    """
+    calculates x,y,z coordinates of a point on a sphere with
+    radius R = EARTH_RADIUS_METERS
+    """
+    lon_r = np.radians(lon)
+    lat_r = np.radians(lat)
+
+    x = R * np.cos(lat_r) * np.cos(lon_r)
+    y = R * np.cos(lat_r) * np.sin(lon_r)
+    z = R * np.sin(lat_r)
+    return x, y, z
+
+
+def cartesian_to_lon_lat(x):
+    """
+     x - vector with coordinates [x1, y1, z1]
+     returns [lon, lat]
+    """
+
+    lon = np.arctan2(x[1], x[0])
+    lon = np.degrees(lon)
+    lat = np.arcsin(x[2] / (np.dot(x, x)) ** 0.5)
+    lat = np.degrees(lat)
+    return lon, lat
+
+
+#nvectors.shape = (3, nx, ny)
+def get_coefs_between(nvectors1, nvectors2):
+    return np.array([1.0 / (get_angle_between_vectors(v1, v2) * EARTH_RADIUS_METERS ) ** 2.0 for v1, v2 in
+                     zip(nvectors1, nvectors2)])
+
+
+if __name__ == "__main__":
+    print "Hello World"
---
+++ src/rpn/domains/rotated_lat_lon.py
@@ -0,0 +1,152 @@
+#
+from rpn.domains import lat_lon
+
+__author__ = 'huziy'
+
+import numpy as np
+
+
+class RotatedLatLon():
+    def __init__(self, lon1=180.0, lat1=0.0, lon2=180.0, lat2=0.0, **kwargs):
+        """
+        Basis vactors of the rotated coordinate system in the original coord system
+        e1 = -p1/|p1|                   =>   row0
+        e2 = -( p2 - (p1, p2) * p1) / |p2 - (p1, p2) * p1| #perpendicular to e1, and lies in
+        the plane parallel to the plane (p1^p2)  => row1
+        e3 = [p1,p2] / |[p1, p2]| , perpendicular to the plane (p1^p2)          => row2
+        """
+
+        self.lon1 = lon1
+        self.lon2 = lon2
+        self.lat1 = lat1
+        self.lat2 = lat2
+
+        self.mean_earth_radius_m_crcm5 = 0.637122e7  # mean earth radius used in the CRCM5 model for area calculation
+
+        p1 = lat_lon.lon_lat_to_cartesian(lon1, lat1, R=1.0)
+        p2 = lat_lon.lon_lat_to_cartesian(lon2, lat2, R=1.0)
+
+        p1 = np.array(p1)
+        p2 = np.array(p2)
+
+        cross_prod = np.cross(p1, p2)
+        dot_prod = np.dot(p1, p2)
+
+        row0 = -np.array(p1) / np.sqrt(np.dot(p1, p1))
+        e2 = (dot_prod * p1 - p2)
+        row1 = e2 / np.sqrt(np.dot(e2, e2))
+        row2 = cross_prod / np.sqrt(np.dot(cross_prod, cross_prod))
+        self.rot_matrix = np.matrix([row0, row1, row2])
+        assert isinstance(self.rot_matrix, np.matrix)
+
+
+    def write_coords_to_rpn(self, rpnObj, x, y):
+        """
+        x, y - 1d coordinates in rotated system
+        """
+        x = np.array(x)
+        x.shape = (len(x), 1)
+        y = np.array(y)
+        y.shape = (1, len(y))
+
+        rpnObj.write_2D_field(name="^^", grid_type="E", data=y, typ_var="X", level=0, ip=range(100, 103),
+                              lon1=self.lon1, lat1=self.lat1, lon2=self.lon2, lat2=self.lat2, label="")
+
+        rpnObj.write_2D_field(name=">>", grid_type="E", data=x, typ_var="X", level=0, ip=range(100, 103),
+                              lon1=self.lon1, lat1=self.lat1, lon2=self.lon2, lat2=self.lat2, label="")
+
+        info = rpnObj.get_current_info
+        ip_xy = map(lambda x: x.value, info["ip"])
+        ig = ip_xy + [0]
+        return ig
+
+
+    def toProjectionXY(self, lon, lat):
+        """
+        Convert geographic lon/lat coordinates to the rotated lat lon coordinates
+        """
+
+        p = lat_lon.lon_lat_to_cartesian(lon, lat, R=1)
+        p = self.rot_matrix * np.mat(p).T
+        return lat_lon.cartesian_to_lon_lat(p.A1)
+
+
+    def toGeographicLonLat(self, x, y):
+        """
+        convert geographic lat / lon to rotated coordinates
+        """
+        p = lat_lon.lon_lat_to_cartesian(x, y, R=1)
+        p = self.rot_matrix.T * np.mat(p).T
+        return lat_lon.cartesian_to_lon_lat(p.A1)
+
+
+    def get_areas_of_gridcells(self, dlon, dlat, nx, ny, latref, jref):
+        """
+        dlon, dlat, lonref, latref - are in degrees
+        iref and jref are counted starting from 1, put here direct values grom gemclim_settings.nml
+        """
+        dx = np.radians(dlon)
+        dy = np.radians(dlat)
+
+        latref_rad = np.radians(latref)
+
+        lats2d = np.zeros((nx, ny))
+        for j in range(ny):
+            lat = latref_rad + (j - jref + 1) * dy
+            lats2d[:, j] = lat
+
+        return self.mean_earth_radius_m_crcm5 ** 2 * np.cos(lats2d) * dx * dy
+
+
+    def get_south_pol_coords(self):
+        rot_pole = self.rot_matrix * np.mat([0, 0, -1]).T
+        return lat_lon.cartesian_to_lon_lat(rot_pole.A1)
+        pass
+
+
+    def get_north_pole_coords(self):
+        """
+        get true coordinates of the rotated north pole
+        """
+        rot_pole = self.rot_matrix * np.mat([0, 0, 1]).T
+        return lat_lon.cartesian_to_lon_lat(rot_pole.A1)
+
+
+    def get_true_pole_coords_in_rotated_system(self):
+        """
+        needed for lon_0 in basemap
+        """
+        rot_pole = self.rot_matrix.T * np.mat([0, 0, 1]).T
+        return lat_lon.cartesian_to_lon_lat(rot_pole.A1)
+
+
+
+    def get_basemap_object_for_lons_lats(self, lons2d = None, lats2d = None, resolution = "i"):
+        from mpl_toolkits.basemap import Basemap
+
+        lon0, _ = self.get_true_pole_coords_in_rotated_system()
+        o_lon_p, o_lat_p = self.get_north_pole_coords()
+        return Basemap(projection="rotpole", lon_0=lon0 - 180, o_lon_p=o_lon_p, o_lat_p=o_lat_p,
+                       llcrnrlon=lons2d[0, 0], llcrnrlat=lats2d[0, 0],
+                       urcrnrlon=lons2d[-1, -1], urcrnrlat=lats2d[-1, -1], resolution = resolution)
+
+
+
+
+def main():
+    rll = RotatedLatLon(lon1=-68, lat1=52, lon2=16.65, lat2=0.0)
+    print rll.rot_matrix
+
+    prj = rll.toProjectionXY(0, 0)
+    print prj
+    print rll.toGeographicLonLat(prj[0], prj[1])
+
+
+    #TODO: implement
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    print "Hello world"
+
