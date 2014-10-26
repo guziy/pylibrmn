0d955a3860929133949dabe4057b3cd515a18c55
Huziy Oleksandr <guziy.sasha@gmail.com>
2014-10-26 16:40:31 -0400

    Add tests for writing compressed floating point outputs

--- src/rpn/tests/test_write.py
+++ src/rpn/tests/test_write.py
@@ -1,4 +1,5 @@
 import subprocess
+from rpn import data_types
 
 __author__ = 'huziy'
 
@@ -9,7 +10,7 @@ import os
 from nose.tools import ok_
 
 
-def test_write_rpn():
+def test_write_rpn_32():
     wfile = "test.rpn"
     r = RPN(wfile, mode="w")
     nx = ny = 10
@@ -31,6 +32,34 @@ def test_write_rpn():
     ok_("{:E}".format(arr.max()) in out, "Could not find the max={:E} in {}".format(arr.max(), out))
     ok_("{:E}".format(arr.min()) in out, "Could not find the min={:E} in {}".format(arr.min(), out))
     ok_("{:E}".format(arr.mean()) in out, "Could not find the mean={:E} in {}".format(arr.mean(), out))
+    ok_("{}".format(32) in out, "Could not find 32 in the ggstat output")
+    print "{:E}".format(arr.mean()), "{:E}".format(arr.min()), "{:E}".format(arr.max())
+
+    os.remove(wfile)
+
+def test_write_rpn_compressed():
+    wfile = "test.rpn"
+    r = RPN(wfile, mode="w")
+    nx = ny = 10
+    arr = np.zeros((nx, ny), dtype=np.float32)
+    for i in range(nx):
+        for j in range(ny):
+            arr[i, j] = i ** 2 + j ** 2
+
+    r.write_2D_field(
+        name="TEST", data=arr, data_type=data_types.compressed_floating_point, nbits=-16
+    )
+    r.close()
+
+    proc = subprocess.Popen(["r.diag", "ggstat", wfile], stdout=subprocess.PIPE)
+    (out, err) = proc.communicate()
+
+    print out
+
+    ok_("{:E}".format(arr.max()) in out, "Could not find the max={:E} in {}".format(arr.max(), out))
+    ok_("{:E}".format(arr.min()) in out, "Could not find the min={:E} in {}".format(arr.min(), out))
+    ok_("{:E}".format(arr.mean()) in out, "Could not find the mean={:E} in {}".format(arr.mean(), out))
+    ok_("{}".format(16) in out, "Could not find 16 in the ggstat output")
     print "{:E}".format(arr.mean()), "{:E}".format(arr.min()), "{:E}".format(arr.max())
 
     os.remove(wfile)
@@ -39,4 +68,4 @@ def test_write_rpn():
 
 
 if __name__ == '__main__':
-    test_write_rpn()
+    test_write_rpn_32()
